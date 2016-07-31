de566a6ff5c6ffaa034feefed27c84cb4bd8ce59
Spencer Bartz <spencerbartz@gmail.com>
2016-07-30 18:49:06 -0700

    separate framework from app I was building

--- index.php
+++ index.php
@@ -1,30 +1,19 @@
 <?php
     include "util/util.php";
-    include_once "controller/welcome_controller.php";
-    $siteRootPath = getAppRoot(__FILE__);
-    printPageDec($siteRootPath);
+    include_once "controller/abstract_controller.php";
+    $siteRootPath = get_relative_root_path	(__FILE__);
+    print_page_dec($siteRootPath);
 ?>
 </head>
 <body>
 <div id="header-wrapper">
+	 <h1>Users</h1>	
     <div id="header" class="container">
-        <div id="logo">
-            <h1><a href="http://www.horrieinternational.com"> < Back to Horrie International</a></h1>
-        </div>
-        <?php printAppMenu($siteRootPath); ?>
     </div>
     
     <div id="banner" class="container">
-	<div class="title">
-                <h2>Create Your Own Adventure!</h2>
-                <span class="byline">The possibilities are endless...</span>
-        </div>
-        <ul class="actions">
-            <li>Redirecting you to our welcome page now ...</li>
-            <meta http-equiv="refresh" content="0;URL=controller/welcome_controller.php?controller=welcome&action=welcome_landing"></li>
-        </ul>
     </div>
 </div>
-<?php printCopyright(); ?>
+<?php print_copy_right(); ?>
 </body>
 </html>
--- util/util.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function()
-{
-    fx = new FX();
-    fx.wireEvents();
-});

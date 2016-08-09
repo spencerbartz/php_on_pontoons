56af848848672a3342033ed7f45432a9b74d714b
Spencer Bartz <spencerbartz@gmail.com>
2016-08-09 00:30:58 -0700

    Fix weird auto-spacing from IDE

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-    define("SLASH", DIRECTORY_SEPARATOR);
-    define("CONTROLLER_DIR", dirname( __FILE__ ) . SLASH . 'controller' . SLASH);
-    define("UTIL_DIR", dirname(__FILE__) . SLASH . "util" . SLASH);
-    define("IMG_DIR", dirname(__FILE__) . SLASH . "img" . SLASH);
-    include "util/string_util.php";
+	define("SLASH", DIRECTORY_SEPARATOR);
+	define("CONTROLLER_DIR", dirname( __FILE__ ) . SLASH . 'controller' . SLASH);
+	define("UTIL_DIR", dirname(__FILE__) . SLASH . "util" . SLASH);
+	define("IMG_DIR", dirname(__FILE__) . SLASH . "img" . SLASH);
+	include "util/string_util.php";
 	include "util/path_util.php";
 	include "util/dbconnect.php";
-    include "util/util.php";
-    include "router/routes.php";
+	include "util/util.php";
+	include "router/routes.php";
 ?>
